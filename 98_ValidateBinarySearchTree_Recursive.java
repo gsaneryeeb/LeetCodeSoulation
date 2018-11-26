405aa7259678389dcea05c300d9888863385b86c
saneryee <gsangeryeee@gmail.com>
2018-11-26 11:15:04 +0800

    Update 98_ValidateBinarySearchTree_Recursive.java
    Java Recurisive Solution
    Time: O(N)

---
+++ 98_ValidateBinarySearchTree_Recursive.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root){
+        // For special test case [2147483647], we need use Long instead of Integer.
+        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    public boolean isValid(TreeNode root, Long min, Long max){
+        if (root == null) return true;
+        if (root.val <= min || root.val >= max) return false;
+        return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+    }
+}
